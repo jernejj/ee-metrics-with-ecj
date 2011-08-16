d6c62b9e08e7192501887ec70140970d3c40eafc
jernej.jerebic@gmail.com <jernej.jerebic@gmail.com@14446edf-a0f7-2f34-9d7e-60554af693df>
2011-08-16 18:50:47 +0000

    DE samples added

---
+++ trunk/ecj/ec/EEstat/samples/DE/Sphere.java
@@ -0,0 +1,37 @@
+package ec.EEstat.samples.DE;
+
+import ec.EvolutionState;
+import ec.Individual;
+import ec.Problem;
+import ec.gp.koza.KozaFitness;
+import ec.simple.SimpleProblemForm;
+import ec.vector.DoubleVectorIndividual;
+
+public class Sphere extends Problem implements SimpleProblemForm 
+{
+
+	@Override
+	public void evaluate(EvolutionState state, Individual ind,
+			int subpopulation, int threadnum) 
+	{
+        
+        if( !( ind instanceof DoubleVectorIndividual ) )
+            state.output.fatal( "The individuals for this problem should be DoubleVectorIndividuals." );
+
+        double[] genome = ((DoubleVectorIndividual)ind).genome;
+        int len = genome.length;
+        float value = 0;
+
+        // Compute the Sphere function for our genome
+       for(int i = 0; i < len; i++)
+       {
+	        value += Math.pow(genome[i], 2);
+       }
+
+        ((KozaFitness)(ind.fitness)).setStandardizedFitness(state, value);
+    
+        ind.evaluated = true;
+
+	}
+
+}
