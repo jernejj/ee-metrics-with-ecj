57260a3f6201c04a9d21efb532d3b33f2c8da734
jernej.jerebic@gmail.com <jernej.jerebic@gmail.com@14446edf-a0f7-2f34-9d7e-60554af693df>
2011-08-16 18:50:47 +0000

    DE samples added

---
+++ ecj/ec/EEstat/samples/DE/Schwefel.java
@@ -0,0 +1,40 @@
+package ec.EEstat.samples.DE;
+
+import ec.EvolutionState;
+import ec.Individual;
+import ec.Problem;
+import ec.EEstat.FloatVectorIndividualStat;
+import ec.gp.koza.KozaFitness;
+import ec.simple.SimpleProblemForm;
+
+public class Schwefel extends Problem implements SimpleProblemForm 
+{
+
+	@Override
+	public void evaluate(EvolutionState state, Individual ind,
+			int subpopulation, int threadnum)
+	{
+		
+		
+		if( !( ind instanceof FloatVectorIndividualStat ) )
+            state.output.fatal( "The individuals for this problem should be DoubleVectorIndividuals." );
+
+        float[] genome = ((FloatVectorIndividualStat)ind).genome;
+        int len = genome.length;
+        float value = 0;
+
+        // Compute the Sphere function for our genome
+       for(int i = 0; i < len; i++)
+       {
+	        value += genome[i] * Math.sin( Math.sqrt(Math.abs(genome[i])) );
+       }
+       
+       value = (float)418.9829 * len + value;
+
+        ((KozaFitness)(ind.fitness)).setStandardizedFitness(state, value);
+    
+        ind.evaluated = true;
+
+	}
+
+}
