bfdda83bf9e7da3243550c9edf7bce8ca72d1985
jernej.jerebic@gmail.com <jernej.jerebic@gmail.com@14446edf-a0f7-2f34-9d7e-60554af693df>
2012-07-24 07:11:58 +0000

    minor changes

--- ecj/ec/EEstat/samples/DE/ECSuite.java
+++ ecj/ec/EEstat/samples/DE/ECSuite.java
@@ -141,6 +141,7 @@ public class ECSuite extends Problem implements SimpleProblemForm
 	        
 	        if (countEval == numOfEval)
 	        {
+	        	state.output.println( "reach evaluation limit, finishing...", 0);
 	        	state.finisher.finishPopulation(state, ec.EvolutionState.R_SUCCESS);
 	        	isOptimal = true;
 	        }
